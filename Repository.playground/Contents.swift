abf3501aa7546b28cad6632ffe2203e02632b0c0
Grant <grantsrockets@yahoo.com>
2020-10-31 23:35:17 -0700

    complete

--- Repository.playground/Contents.swift
+++ Repository.playground/Contents.swift
@@ -1,45 +1,93 @@
 import UIKit
 
-class UserRepository {
+class MusicRepository {
     var path: String
     init(withPath path:String){
         self.path = path
     }
     // READ a single object
-    func fetch(withId id: Int, withCompletion completion: @escaping (User?) -> Void) {
-        let URLstring = path + "\(id)"
+    func fetch(withId id: Int, withCompletion completion: @escaping (Music?) -> Void) {
+        let URLstring = path + "music/id/\(id)"
         if let url = URL.init(string: URLstring){
             let task = URLSession.shared.dataTask(with: url, completionHandler:
             {(data, response, error) in
-                if let user = try? JSONDecoder().decode(User.self, from: data!){
+                if let user = try? JSONDecoder().decode(Music.self, from: data!){
                     completion (user)
                 }
             })
+            
             task.resume()
         }
     }
     
     //TODO: Build and test comparable methods for the other CRUD items
-    //func create( a:User , withCompletion completion: @escaping (User?) -> Void) {}
-    //func update( withId id:Int, a:User) {}
-    //func delete( withId id:Int ) {}
+    
+    func create( a:Music , withCompletion completion: @escaping (Music?) -> Void) {
+        let URLstring = path + "music/id/\(a.id!)"
+        var request = URLRequest.init(url: URL.init(string: URLstring)!)
+       
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONEncoder().encode(a)
+        
+        let task = URLSession.shared.dataTask(with: request) {data, response, _ in
+           
+            if let music = try? JSONDecoder().decode(Music.self, from: data!){
+                completion (music)
+            }
+        }
+        
+        task.resume()
+        
+    }
+    
+    func update( withId id:Int, a:Music) {
+    
+        let URLstring = path + "music/id/\(a.id!)"
+        var request = URLRequest.init(url: URL.init(string: URLstring)!)
+       
+        request.httpMethod = "PUT"
+        request.httpBody = try? JSONEncoder().encode(a)
+        
+        let task = URLSession.shared.dataTask(with: request) {data, response, _ in
+            print (String.init(data: data!, encoding: .ascii) ?? "no data")
+            
+        }
+        
+        task.resume()
+        
+}
+    
+    func delete( withId id:Int ) {
+        
+        let URLstring = path + "music/id/\(id)"
+        var request = URLRequest.init(url: URL.init(string: URLstring)!)
+       
+        request.httpMethod = "DELETE"
+                
+        let task = URLSession.shared.dataTask(with: request) {data, response, _ in
+            print (String.init(data: data!, encoding: .ascii) ?? "no data")
+            
+        }
+        
+        task.resume()
+        
+    }
     
 }
 
-class User: Codable {
-    var UserID: String?
-    var FirstName: String?
-    var LastName: String?
-    var PhoneNumber: String?
-    var SID: String?
+class Music: Codable {
+    var id: String?
+    var music_url: String?
+    var name: String?
+    var description: String?
+
 }
 
 //Create a User Repository for the API at http://216.186.69.45/services/device/users/
-let userRepo = UserRepository(withPath: "http://216.186.69.45/services/device/users/")
-
+let musicRepo = MusicRepository(withPath: "https://www.orangevalleycaa.org/api/music")
 //Fetch a single User
-userRepo.fetch(withId: 43, withCompletion: {(user) in
-        print(user!.FirstName ?? "no user")
+musicRepo.fetch(withId: 43, withCompletion: {(music) in
+        print(music!.id ?? "no music")
 })
 
 /**
